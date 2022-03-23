ffbda129da0b2cf3a375c39a7d957467ac2502eb
NestaCbs27 <97601193+NestaCbs27@users.noreply.github.com>
2022-03-23 17:39:43 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 
 $( document ).ready(function() {
     navigation('new')
-	
-	
 });
 
 if ("serviceWorker" in navigator) {
@@ -25,11 +23,28 @@ function navigation(id){
 	$('.currentContainer').addClass('hidden');
 	$('.listContainer').addClass('hidden');
 	
-	
-	
 	$('#'+id).addClass('active');	
 	$('.'+id+'Container').removeClass('hidden')
-	
-	
+		
+}
+
+var joueur1 = "";
+var joueur2 = "";
+
+function startMatch(){
+
+	$('#erreurSaisieJoueurs').addClass('hidden');	
+
+	joueur1 = $('#J1').val();	
+	joueur2 = $('#J2').val();	
+
+	if(joueur1 === "" || joueur2 === ""){
+		$('#erreurSaisieJoueurs').removeClass('hidden');	
+	}else{
+		navigation('current')
+		 $('.J1Name').text(joueur1);	
+		 $('.J2Name').text(joueur2);	
+	}
+
 	
 }
