d0ebec9098f0e3f38bed307da2c0b920eba1c0bb
NestaCbs27 <97601193+NestaCbs27@users.noreply.github.com>
2022-04-11 13:51:54 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,50 +1,72 @@
 
 $( document ).ready(function() {
-    navigation('new')
+	navigation('new')
+	disableMenu('current')
 });
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
+	window.addEventListener("load", function() {
+		navigator.serviceWorker
+		.register("/serviceWorker.js")
+		.then(res => console.log("service worker registered"))
+		.catch(err => console.log("service worker not registered", err))
+	})
 }
 
 
 function navigation(id){	
-	
-	$('#new').removeClass('active');
-	$('#current').removeClass('active');
-	$('#list').removeClass('active');
-	
-	$('.newContainer').addClass('hidden');
-	$('.currentContainer').addClass('hidden');
-	$('.listContainer').addClass('hidden');
-	
-	$('#'+id).addClass('active');	
-	$('.'+id+'Container').removeClass('hidden')
+	if(!$('#'+id).hasClass('disabledMenu')){
+		$('#new').removeClass('active');
+		$('#current').removeClass('active');
+		$('#list').removeClass('active');
 		
+		$('.newContainer').addClass('hidden');
+		$('.currentContainer').addClass('hidden');
+		$('.listContainer').addClass('hidden');
+		
+		
+		$('#'+id).addClass('active');	
+		$('.'+id+'Container').removeClass('hidden')
+	}	
+}
+
+function disableMenu(id){
+	$('#'+id).addClass('disabledMenu');
+}
+
+function enableMenu(id){
+	$('#'+id).removeClass('disabledMenu');
+}
+
+function selectCote(elem){
+	
+	if(elem === 'btnG'){
+		$('#btnD').removeClass('btn-action-active')
+		$('#btnG').addClass('btn-action-active')
+	}
+
+	if(elem === 'btnD'){
+		$('#btnD').addClass('btn-action-active')
+		$('#btnG').removeClass('btn-action-active')
+	}
 }
 
 var joueur1 = "";
 var joueur2 = "";
 
 function startMatch(){
-
+	
 	$('#erreurSaisieJoueurs').addClass('hidden');	
-
+	
 	joueur1 = $('#J1').val();	
 	joueur2 = $('#J2').val();	
-
+	
 	if(joueur1 === "" || joueur2 === ""){
 		$('#erreurSaisieJoueurs').removeClass('hidden');	
 	}else{
+		enableMenu('current')
 		navigation('current')
-		 $('.J1Name').text(joueur1);	
-		 $('.J2Name').text(joueur2);	
+		$('.J1Name').text(joueur1);	
+		$('.J2Name').text(joueur2);	
 	}
-
-	
 }
